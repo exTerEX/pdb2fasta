e806e31c71478a852632d58a7aa3e01263a21dd8
exTerEX <30706568+exTerEX@users.noreply.github.com>
2021-04-03 16:03:56 +0200

    Indicate c++ to treat code as c

--- include/pdb2fasta/pdb2fasta.h
+++ include/pdb2fasta/pdb2fasta.h
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 void *pdb2fasta(FILE *file, const char *name, char *out);
 
-#endif
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // PDB2FASTA_H
