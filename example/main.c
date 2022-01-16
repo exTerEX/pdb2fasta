570ddf7152aff481340d901631e7f5989195a6ad
exTerEX <30706568+exTerEX@users.noreply.github.com>
2022-01-16 10:11:08 +0100

    Formatted

--- example/main.c
+++ example/main.c
@@ -5,55 +5,62 @@
 
 #define BASE_LENGTH 50
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("pdb2fasta /path/to/pdb > /path/to/fasta\n");
-        return argc;
-    }
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
+    printf("pdb2fasta /path/to/pdb > /path/to/fasta\n");
+    return argc;
+  }
 
-    // Read file to stream file
-    FILE *file = fopen(argv[1], "r");
+  // Read file to stream file
+  FILE *file = fopen(argv[1], "r");
 
-    // Check if file exists
-    if (file == NULL) {
-        fprintf(stderr, "ERROR! no such file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
+  // Check if file exists
+  if (file == NULL)
+  {
+    fprintf(stderr, "ERROR! no such file %s\n", argv[1]);
+    return EXIT_FAILURE;
+  }
 
-    int start_index = 0, end_index = strlen(argv[1]);
-    for (unsigned i = 0; i < strlen(argv[1]); i++) {
-        if (argv[1][i] == '/' || argv[1][i] == '\\') {
-            start_index = i + 1;
-            end_index = strlen(argv[1]);
-        }
-        if (argv[1][i] == '.' && end_index == strlen(argv[1])) {
-            end_index = i;
-        }
-    }
-
-    char name[end_index - start_index + 1];
-    for (unsigned i = start_index; i < end_index; i++) {
-        name[i - start_index] = argv[1][i];
-    }
-    name[end_index - start_index] = '\0';
-
-    if (strlen(name) >= BASE_LENGTH)
+  int start_index = 0, end_index = strlen(argv[1]);
+  for (unsigned i = 0; i < strlen(argv[1]); i++)
+  {
+    if (argv[1][i] == '/' || argv[1][i] == '\\')
     {
-      name[BASE_LENGTH] = '\0';
+      start_index = i + 1;
+      end_index = strlen(argv[1]);
     }
+    if (argv[1][i] == '.' && end_index == strlen(argv[1]))
+    {
+      end_index = i;
+    }
+  }
 
-    char *content = malloc(sizeof(*content) * 1);
+  char name[end_index - start_index + 1];
+  for (unsigned i = start_index; i < end_index; i++)
+  {
+    name[i - start_index] = argv[1][i];
+  }
+  name[end_index - start_index] = '\0';
 
-    content = pdb2fasta(file, name, content);
+  if (strlen(name) >= BASE_LENGTH)
+  {
+    name[BASE_LENGTH] = '\0';
+  }
 
-    // Write converted output to file.
-    FILE *out = fopen(argv[2], "w");
-    fprintf(out, "%s", content);
-    fclose(out);
+  char *content = malloc(sizeof(*content) * 1);
 
-    free(content);
+  content = pdb2fasta(file, name, content);
 
-    fclose(file);
+  // Write converted output to file.
+  FILE *out = fopen(argv[2], "w");
+  fprintf(out, "%s", content);
+  fclose(out);
 
-    return (EXIT_SUCCESS);
+  free(content);
+
+  fclose(file);
+
+  return (EXIT_SUCCESS);
 }
