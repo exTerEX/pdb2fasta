f5e63c45c894bf4772773e607dcd7d6cf326a5e3
exTerEX <30706568+exTerEX@users.noreply.github.com>
2022-01-16 12:24:31 +0100

    Created a CLI tool

--- extras/cli.c
+++ extras/cli.c
@@ -1,45 +1,112 @@
 #include "pdb2fasta/pdb2fasta.h"
 #include <argp.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BASE_LENGTH 50
 
 const char *argp_program_version = "pdb2fasta 0.1.0";
 const char *argp_program_bug_address = "developer@sagen.io";
 static char doc[] = "Converts PDB to fasta files.";
-static char args_doc[] = "pdb2fasta...";
 static struct argp_option options[] = {
-  {"line", 'l', 0, 0, "Compare lines instead of characters."},
-  {"word", 'w', 0, 0, "Compare words instead of characters."},
-  {"nocase", 'i', 0, 0, "Compare case insensitive instead of case sensitive."},
+  {"input", 'i', "FILE", 0, "Path to input (PDB) file."},
+  {"output", 'o', "FILE", OPTION_ARG_OPTIONAL, "Path to output (FASTA) file."},
+  {"name", 'n', 0, OPTION_ARG_OPTIONAL, "Name identifier in FASTA file."},
   {0}};
 
-struct arguments
-{
-  enum { CHARACTER_MODE, WORD_MODE, LINE_MODE } mode;
-  bool isCaseInsensitive;
+struct arguments {
+  char *input_file;
+  char *output_file;
+  char *file_name;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
   struct arguments *arguments = state->input;
+
   switch (key)
   {
-  case 'l': arguments->mode = LINE_MODE; break;
-  case 'w': arguments->mode = WORD_MODE; break;
-  case 'i': arguments->isCaseInsensitive = true; break;
+  case 'i': arguments->input_file = arg; break;
+  case 'o': arguments->output_file = arg; break;
+  case 'n': arguments->file_name = arg; break;
   case ARGP_KEY_ARG: return 0;
   default: return ARGP_ERR_UNKNOWN;
   }
+
   return 0;
 }
 
-static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
+static struct argp argp = {options, parse_opt, 0, doc, 0, 0, 0};
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
   struct arguments arguments;
 
-  arguments.mode = CHARACTER_MODE;
-  arguments.isCaseInsensitive = false;
-
   argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+  FILE *file = fopen(arguments.input_file, "r");
+
+  if (file == NULL)
+  {
+    fprintf(stderr, "ERROR: No file named \'%s\'\n", arguments.input_file);
+    return EXIT_FAILURE;
+  }
+
+  char *content = malloc(sizeof(*content) * 1);
+
+  if (!(arguments.file_name == NULL))
+  {
+    int start_index = 0, end_index = strlen(arguments.input_file);
+    for (unsigned i = 0; i < strlen(arguments.input_file); i++)
+    {
+      if (arguments.input_file[i] == '/' || arguments.input_file[i] == '\\')
+      {
+        start_index = i + 1;
+        end_index = strlen(arguments.input_file);
+      }
+      if (arguments.input_file[i] == '.' &&
+          end_index == strlen(arguments.input_file))
+      {
+        end_index = i;
+      }
+    }
+
+    char file_name[end_index - start_index + 1];
+    for (unsigned i = start_index; i < end_index; i++)
+    {
+      file_name[i - start_index] = arguments.input_file[i];
+    }
+    file_name[end_index - start_index] = '\0';
+
+    if (strlen(file_name) >= BASE_LENGTH)
+    {
+      file_name[BASE_LENGTH] = '\0';
+    }
+
+    content = pdb2fasta(file, file_name, content);
+  }
+  else
+  {
+    // TODO: Correct/warn if name is longer then 50 chars
+    content = pdb2fasta(file, arguments.file_name, content);
+  }
+
+  if (!(arguments.output_file == NULL))
+  {
+    printf("%s", content);
+  }
+  else
+  {
+    FILE *out = fopen(arguments.output_file, "w");
+    fprintf(out, "%s", content);
+    fclose(out);
+  }
+
+  free(content);
+
+  fclose(file);
+
+  return (EXIT_SUCCESS);
 }
